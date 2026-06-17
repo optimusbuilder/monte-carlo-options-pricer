028171ee8eeff629349b4319f4f1b241343f7229
Oluwaferanmi Oyelude <oyeludeferanmi@gmail.com>
2026-06-16 23:52:47 -0400

    latest

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include <string>
 #include <iostream>
+#include <random>
 using namespace std ;
 
 
 struct MonteCarloPricer {
-    int currentStockPrice;
-    int optionPrice;
-    string priceExpiry;
-    int stockVolatility;
-    int numberOfSimulations;
-
+    
 };
+
+int main() {
+    mt19937 mt{ static_cast<std::mt19937::result_type>(
+		chrono::steady_clock::now().time_since_epoch().count()
+		) };
+    double currentStockPrice;
+    double optionPrice;
+    double priceExpiry;
+    double stockVolatility;
+    int numberOfSimulations;
+    double riskFreeRate;
+
+    double simulate(double currentStockPrice, double optionPrice, double priceExpiry, double stockVolatility, int numSimulations, double riskFreeRate) {
+        double mean = 0.0;
+        double standardDeviation = 1.0;
+        normal_distribution<double> numGen{mean, standard_deviation};
+        };
+    return 0;
+}
